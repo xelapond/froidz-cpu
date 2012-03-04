d8db0cfafca9e1d2b04cb922de6b12792c024b2e
Jacob Weiss <jacob@radagast-2.local>
2012-03-04 15:48:58 -0800

    ChunkBag

--- java/WPParser.java
+++ java/WPParser.java
@@ -1,3 +1,10 @@
+import java.util.HashMap;
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * Parses a wp file.
  * 
@@ -6,13 +13,50 @@
  */
 public class WPParser
 {
-    private int x;
+    HashMap<String, WPChunk> chunkBag = new HashMap();
 
     //Public methods
     // get_opcode - Used by assember
     // get_pattern - Used by simulator
 
-    /**
-     * 
-     */
+    public WPParser(String path)
+    {
+        for (String line : this.removeComments(this.getLines(path)))
+        {
+            WPChunk chunk = new WPChunk(line);
+            chunkBag.put(chunk.getOpName(), chunk);
+        }
+    }
+    
+    public List<String> getLines(String path)
+    {
+        LinkedList<String> lines = new LinkedList();
+        Scanner scanner;
+        try
+        {
+            scanner = new Scanner(new File(path));
+            while (scanner.hasNextLine())
+            {
+                lines.add(scanner.nextLine());
+            }
+        }
+        catch (Throwable e)
+        {
+            System.out.println("File not found.");
+        }
+        return lines;
+    }
+    
+    private List<String> removeComments(List<String> lines)
+    {
+        Iterator<String> iterator = lines.iterator();
+        while (iterator.hasNext())
+        {
+            if (iterator.next().indexOf("#") == 0)
+            {
+                iterator.remove();
+            }
+        }
+        return lines;
+    }
 }
