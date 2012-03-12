e86e3403ee6751488eec96a9f07aba120079bcdf
Alex Teiche <xelapond@gmail.com>
2012-03-11 17:57:22 -0700

    Added CPU Files

---
+++ cpu/src/TC.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class ToastyConstants here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TC
+{
+    // Locations in IO Space
+    public static final int SREG = 0;
+    public static final int SP = 1;
+    
+    // Instruction Op-Codes
+    public static final int ADD = 1;
+    public static final int LDI = 8;
+    public static final int EOF = Integer.MAX_VALUE;
+}
